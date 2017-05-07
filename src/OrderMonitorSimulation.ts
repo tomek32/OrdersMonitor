321fe877e53df8b3c8efcd4e3b04405764c378c4
Tom <tomek32@gmailc.om>
2017-05-07 08:45:22 -0400

    1. Fixed export to csv for the orders report

--- src/OrderMonitorSimulation.ts
+++ src/OrderMonitorSimulation.ts
@@ -10,10 +10,10 @@ const fs = require('fs');
 const json2csv = require('json2csv');
 const writeJsonFile = require('write-json-file');
 
-const orderReportJson: string = './output/orders_report.json';
-const orderReportCsv: string  = './output/orders_report.csv';
-const exceptionReportJson: string  = './output/order_exceptions.json';
-const exceptionReportCsv: string  = './output/order_exceptions.csv';
+const orderReportJsonFile: string = './output/orders_report_sample.json';
+const orderReportCsvFile: string  = './output/orders_report_sample.csv';
+const exceptionReportJsonFile: string  = './output/order_exceptions_sample.json';
+const exceptionReportCsvFile: string  = './output/order_exceptions_sample.csv';
 
 const orderExceptionMaxSecs: number = 60 * 10;
 
@@ -35,19 +35,72 @@ let simulationCallback = function () {
 function exportReports() {
   let json: any, csv: any;
 
-  // Export monitor report
-  json = orderMonitor.getReport();
-  csv = json2csv({data: json});
+  // Export orders report
+  writeJsonFile(orderReportJsonFile, orderMonitor.getReport()).then(() => {});
 
-  writeJsonFile(orderReportJson, json).then(() => {});
-  fs.writeFile(orderReportCsv, csv, function(err) {});
+  csv = json2csv(getExportOrdersReport());
+  fs.writeFile(orderReportCsvFile, csv, function(err) {});
 
   // Export order exceptions
-  json = orderStream.getOrderExceptions();
-  csv = json2csv({data: json});
+  writeJsonFile(exceptionReportJsonFile, orderStream.getOrderExceptions()).then(() => {});
 
-  writeJsonFile(exceptionReportJson, json).then(() => {});
-  fs.writeFile(exceptionReportCsv, csv, function(err) {});
+  //TODO: add export of order exceptions
+  //csv = json2csv({data: json});
+  //fs.writeFile(exceptionReportCsvFile, csv, function(err) {});
+}
+
+function getExportOrdersReport(): any {
+  let report: any, json: any, dateReport: any;
+  let fields: string[], fieldNames: string[];
+
+  // Export monitor report
+  report = orderMonitor.getReport();
+  fields = ['Date',
+    'All',
+    'WEB', 'WBR', 'ATT', 'MBL', 'TMAX', 'TALK', 'TBL', 'UNKNOWN',
+    'RMPA', 'RMP1', 'RMP2', 'RMP3', 'RMP4', 'RM',
+    'SPL', 'OCO', 'OTA', 'FTO', 'MLO',
+    'MARKET', 'LIMIT', 'STOP_MARKET', 'STOP_LIMIT', 'TRAILING_STOP_MARKET', 'TRAILING_STOP_LIMIT',
+    'ATT_RMP1'];
+  fieldNames = ['Date',
+    'All Channels',
+    'WebBroker Legacy', 'WebBroker', 'Advanced Dashboard', 'Mobile', 'Telemax', 'TalkBroker', 'Tablet', 'Unknown',
+    'RMPA', 'RMP1', 'RMP2', 'RMP3', 'RMP4', 'RM**',
+    'Simple', 'OCO', 'OTA', 'FTO', 'Multi-Leg',
+    'Market', 'Limit', 'Stop Market', 'Stop Limit', 'Trailing Stop Market', 'Trailing Stop Limit',
+    'AD + RMP1'];
+
+  json = [];
+  Object.keys(report).forEach(dateKey => {
+    dateReport = {};
+    dateReport.Date = dateKey;
+    dateReport.All = report[dateKey].numOrders;
+
+    Object.keys(report[dateKey].numOrdersByChannel).forEach(channelKey => {
+      dateReport[channelKey] = report[dateKey].numOrdersByChannel[channelKey];
+    });
+    Object.keys(report[dateKey].numOrdersByRRCode).forEach(rrCodeKey => {
+      dateReport[rrCodeKey] = report[dateKey].numOrdersByRRCode[rrCodeKey];
+    });
+    Object.keys(report[dateKey].numOrdersByChannel).forEach(channelKey => {
+      dateReport[channelKey] = report[dateKey].numOrdersByChannel[channelKey];
+    });
+    Object.keys(report[dateKey].numOrdersByStrategy).forEach(strategyKey => {
+      dateReport[strategyKey] = report[dateKey].numOrdersByStrategy[strategyKey];
+    });
+    Object.keys(report[dateKey].numOrdersByOrderType).forEach(orderTypeKey => {
+      dateReport[orderTypeKey] = report[dateKey].numOrdersByOrderType[orderTypeKey];
+    });
+
+    dateReport.ATT_RMP1 = report[dateKey].ATT_RMP1;
+
+    if (dateKey == 'totals')
+      dateReport['Date'] = 'Totals';
+
+    json.push(dateReport);
+  });
+
+  return {data: json, fields: fields, fieldNames: fieldNames};
 }
 
 let orderMonitor = new OrderMonitor(orderExceptionMaxSecs, OrderMarketHours.ALL);
